ab4afdac1276178187cdde1ae07802b72ef34f18
Sjoerd <svanhouten@gmail.com>
2022-04-09 14:23:35 +0200

    ch 4, improved tests and made amount private

--- src/test/java/com/github/shmvanhouten/tddmoney/MoneyExchangeTestTest.java
+++ src/test/java/com/github/shmvanhouten/tddmoney/MoneyExchangeTestTest.java
@@ -8,21 +8,21 @@ import static org.junit.jupiter.api.Assertions.*;
  * TODO:
  * $5 + 10 CHF = $10 if rate is 2:1
 // * $5 * 2 = $10
- * Make amount private
+// * Make amount private
 // * Dollar side-effects?
  * Money rounding?
 // * equals()
  * hashcode()
+ * Equal null
+ * Equal object
  */
 class MoneyExchangeTest {
 
     @Test
     void testMultiplication() {
         Dollar five = new Dollar(5);
-        Dollar product = five.times(2);
-        assertEquals(10, product.amount);
-        product = five.times(3);
-        assertEquals(15, product.amount);
+        assertEquals(new Dollar(10), five.times(2));
+        assertEquals(new Dollar(15), five.times(3));
     }
 
     @Test
