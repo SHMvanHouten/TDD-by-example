71c1570ac1eee1a9f9aa2e1ea51736fadaa2866b
Sjoerd <svanhouten@gmail.com>
2022-04-09 14:54:18 +0200

    ch 8: step toward elimitating dulication: reconcile signatures of times(), decoupled tests by introducing factory

--- src/main/java/com/github/shmvanhouten/tddmoney/Money.java
+++ src/main/java/com/github/shmvanhouten/tddmoney/Money.java
@@ -1,7 +1,17 @@
 package com.github.shmvanhouten.tddmoney;
 
-public class Money {
+public abstract class Money {
     protected int amount;
+
+    public abstract Money times(int amount);
+
+    public static Money franc(int amount) {
+        return new Franc(amount);
+    }
+
+    public static Money dollar(int amount) {
+        return new Dollar(amount);
+    }
 
     @Override
     public boolean equals(Object other) {
