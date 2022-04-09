51fb702900e634f31b8c4005a4103cf516fedcd3
Sjoerd <svanhouten@gmail.com>
2022-04-09 15:09:09 +0200

    ch 9: tiny steps, introduced currency, worked towards removing duplication

--- src/main/java/com/github/shmvanhouten/tddmoney/Money.java
+++ src/main/java/com/github/shmvanhouten/tddmoney/Money.java
@@ -2,15 +2,25 @@ package com.github.shmvanhouten.tddmoney;
 
 public abstract class Money {
     protected int amount;
+    protected String currency;
+
+    public Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
 
     public abstract Money times(int amount);
 
+    public String currency() {
+        return this.currency;
+    };
+
     public static Money franc(int amount) {
-        return new Franc(amount);
+        return new Franc(amount, "CHF");
     }
 
     public static Money dollar(int amount) {
-        return new Dollar(amount);
+        return new Dollar(amount, "USD");
     }
 
     @Override
--- src/test/java/com/github/shmvanhouten/tddmoney/MoneyExchangeTestTest.java
+++ src/test/java/com/github/shmvanhouten/tddmoney/MoneyExchangeTestTest.java
@@ -20,7 +20,7 @@ import static org.junit.jupiter.api.Assertions.*;
 // * Common equals
  * common times
 // * Compare Francs with Dollars
- * Currency?
+// * Currency?
  * delete testFrancMultiplication?
  */
 class MoneyExchangeTest {
@@ -46,5 +46,11 @@ class MoneyExchangeTest {
         assertTrue(Money.franc(5).equals(Money.franc(5)));
         assertFalse(Money.franc(5).equals(Money.franc(6)));
         assertFalse(Money.franc(5).equals(Money.dollar(5)));
+    }
+
+    @Test
+    void testCurrency() {
+        assertEquals("USD", Money.dollar(1).currency());
+        assertEquals("CHF", Money.franc(1).currency());
     }
 }
